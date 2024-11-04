036f3221ef1ffa6bb0d6c588f18b55e461606f68
lucasoden <113351500+lucasoden@users.noreply.github.com>
2024-11-04 10:45:13 +0100

    Ability Merge Test

---
+++ FlareForge_UE/Source/FlareForge/Public/BasicProjectileAbility.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BasicProjectileAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAREFORGE_API UBasicProjectileAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	//Override the ActivateAbility method
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	FVector BasicProjectileVelocity;
+};
