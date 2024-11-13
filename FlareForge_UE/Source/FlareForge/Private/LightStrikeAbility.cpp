3612f91ed639034cd702dfc353a0cd6f3ea9e76d
ZeroDiego <diegopuentes878@gmail.com>
2024-11-13 17:29:12 +0100

    LightBurst ongoing

--- FlareForge_UE/Source/FlareForge/Private/LightStrikeAbility.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "LightStrikeAbility.h"
-
-void ULightStrikeAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
-{
-	
-}
--- FlareForge_UE/Source/FlareForge/Public/LightBurstAbility.h
+++ FlareForge_UE/Source/FlareForge/Public/LightBurstAbility.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
-#include "LightStrikeAbility.generated.h"
+#include "LightBurstAbility.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class FLAREFORGE_API ULightStrikeAbility : public UGameplayAbility
+class FLAREFORGE_API ULightBurstAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
 
@@ -19,5 +19,14 @@ public:
 	//Override the ActivateAbility method
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
-	
+	UFUNCTION(Server, Reliable)
+	void LightBurstAbility(const FVector SpawnConeLocation, const FRotator CurrentRotation);
+
+private:
+
+	//UPROPERTY(EditAnywhere)
+	//TSubclassOf<ALightBurstCone> ConeBlueprint;
+
+	//UPROPERTY(VisibleAnywhere)
+	//ALightBurstCone* Cone;
 };
