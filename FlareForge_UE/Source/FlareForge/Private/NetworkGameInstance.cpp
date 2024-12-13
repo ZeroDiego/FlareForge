81fb04da81da2d58e6f0f7fea9dde2741697728e
lucasoden <113351500+lucasoden@users.noreply.github.com>
2024-12-13 12:18:50 +0100

    Revert "Reapply "Prövar replication grej""
    This reverts commit 7417f43ecc9d49bea121ac72d08328588ce0c12d.

--- FlareForge_UE/Source/FlareForge/Private/NetworkGameInstance.cpp
+++ FlareForge_UE/Source/FlareForge/Private/NetworkGameInstance.cpp
@@ -65,36 +65,20 @@ TArray<TSubclassOf<UGameplayAbility>> UNetworkGameInstance::GetAbilitiesForPlaye
 	return TArray<TSubclassOf<UGameplayAbility>>();
 }
 
-void UNetworkGameInstance::GetAbilityAtIndexForPlayer_Implementation(const FString& UniquePlayerID, const int32 Index)
+TSubclassOf<UGameplayAbility> UNetworkGameInstance::GetAbilityAtIndexForPlayer_Implementation(const FString& UniquePlayerID, const int32 Index) const
 {
+	// Retrieve the abilities list for the given player ID
 	if (const TArray<TSubclassOf<UGameplayAbility>>* FoundAbilities = PlayerSelectedAbilitiesMap.Find(UniquePlayerID))
 	{
+		// Check if the index is valid
 		if (FoundAbilities->IsValidIndex(Index))
 		{
-			// Call client RPC to send back the ability
-			ClientReceiveAbility(UniquePlayerID, (*FoundAbilities)[Index]);
-			return;
+			return (*FoundAbilities)[Index];
 		}
 	}
-	// Call client RPC with nullptr if index is invalid or abilities not found
-	ClientReceiveAbility(UniquePlayerID, nullptr);
-}
 
-bool UNetworkGameInstance::GetAbilityAtIndexForPlayer_Validate(const FString& UniquePlayerID, const int32 Index)
-{
-	return true; // Add any necessary validation logic here
-}
-
-void UNetworkGameInstance::ClientReceiveAbility_Implementation(const FString& UniquePlayerID, TSubclassOf<UGameplayAbility> Ability)
-{
-	if (Ability)
-	{
-		UE_LOG(LogTemp, Log, TEXT("Received Ability %s for Player %s"), *Ability->GetName(), *UniquePlayerID);
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("No Ability found at given index for Player %s"), *UniquePlayerID);
-	}
+	// Return nullptr if no abilities are found or the index is invalid
+	return nullptr;
 }
 
 void UNetworkGameInstance::SetIsMelee(bool bNewIsMelee)
