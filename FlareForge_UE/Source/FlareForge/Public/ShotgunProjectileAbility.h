c88b3cb38634654b5d321143d03d9cf5d67c204b
lucasoden <113351500+lucasoden@users.noreply.github.com>
2024-12-17 10:01:21 +0100

    Shotgunprojectile start

---
+++ FlareForge_UE/Source/FlareForge/Public/ShotgunProjectileAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "ShotgunProjectileAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAREFORGE_API UShotgunProjectileAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	dsdda
+};
